e7b2022a39a26fcac7fd346fb917949c0eba64a4
markloughlin <christoperw818@gmail.com>
2024-08-04 18:18:54 +0800

    final

--- src/pages/Home/GridSlider.jsx
+++ src/pages/Home/GridSlider.jsx
@@ -1,5 +1,5 @@
 // Card.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Card/Card.css';
 import Card1 from "./Card/Card1";
 import Card2 from "./Card/Card2";
@@ -9,8 +9,20 @@ import Card5 from "./Card/Card5";
 import Card6 from "./Card/Card6";
 
 const GridSlider = ({height}) => {
+
+    const [opacity, setOpacity] = useState(0); // State for opacity
+    useEffect(() => {
+        // Set opacity to 1 after 2 seconds
+        const timer = setTimeout(() => {
+            setOpacity(1);
+        }, 100); // 2 seconds
+
+        return () => clearTimeout(timer); // Cleanup
+    }, []);
+
     return (
-        <div className="grid-container">
+        <div className="grid-container" style={{ opacity, 
+            transition: 'opacity 1s ease-in-out, transform 0.3s ease-in-out' }}>
             <div className="item1"><Card1 height={250}/></div>
             <div className="item2"><Card2 height={450}/></div>
             <div className="item3"><Card3 height={450}/></div>
